b2c2fb95de55222c31871ce19d0db2da9707416c
Facundo Herman Gomez Gottschalk <facundo1988@hotmail.com>
2021-08-12 15:29:13 -0300

    fix: minor fix

--- src/models/service/types.ts
+++ src/models/service/types.ts
@@ -19,30 +19,30 @@ export interface BotService {
 }
 
 export interface Infrastructure {
-    virtualMachineName: string;
-    resourceGroup: string;
-    subscriptionId: string;
-    powerState: BotServiceInfrastructureState;
-    provisioningDetails: ProvisioningDetails;
+  virtualMachineName: string;
+  resourceGroup: string;
+  subscriptionId: string;
+  powerState: BotServiceInfrastructureState;
+  provisioningDetails: ProvisioningDetails;
 }
 
 export interface ProvisioningDetails {
-    state: ProvisioningState;
-    message: string;
+  state: ProvisioningState;
+  message: string;
 }
 
 export interface ProvisioningState {
-   id: ProvisioningStateValues;
-   name: string;
+  id: ProvisioningStateValues;
+  name: string;
 }
 
 export enum ProvisioningStateValues {
-    Provisioning = 0,
-    Provisioned = 1,
-    Deprovisioning = 2,
-    Deprovisioned = 3,
-    Error = 4,
-    Unknown = 5
+  Provisioning = 0,
+  Provisioned = 1,
+  Deprovisioning = 2,
+  Deprovisioned = 3,
+  Error = 4,
+  Unknown = 5
 }
 
 export enum TeamsColors {
@@ -63,9 +63,8 @@ export enum TeamsMargins {
   large = "40px",
 }
 
-export enum BotServiceStates
-{
-    Unavailable = 0,
-    Available = 1,
-    Busy = 2,
+export enum BotServiceStates {
+  Unavailable = 0,
+  Available = 1,
+  Busy = 2,
 }
